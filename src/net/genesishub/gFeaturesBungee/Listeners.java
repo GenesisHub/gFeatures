6f72e502427b55666b4d4e0f0368a565274f27be
_InDev_ <aguy867@gmail.com>
2015-06-26 08:32:54 -0400

    gFeaturesBungee 1.0.0 - It loads.

--- src/net/genesishub/gFeaturesBungee/Listeners.java
+++ src/net/genesishub/gFeaturesBungee/Listeners.java
@@ -1,5 +1,19 @@
 package net.genesishub.gFeaturesBungee;
 
-public class Listeners {
+import net.md_5.bungee.api.plugin.Plugin;
 
+public class Listeners extends Plugin{
+	public static String version = "1.0.0";
+	public void onEnable(){
+		getLogger().info("_______________________________________________");
+		getLogger().info("Enabling gFeaturesBungee version " + version + "...");
+		getLogger().info("Enabled!");
+		getLogger().info("_______________________________________________");
+	}
+	public void onDisable(){
+		getLogger().info("_______________________________________________");
+		getLogger().info("Disabling gFeaturesBungee version " + version + "...");
+		getLogger().info("Disabled!");
+		getLogger().info("_______________________________________________");
+	}
 }
