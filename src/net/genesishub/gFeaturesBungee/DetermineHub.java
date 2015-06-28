dfec3b91fada435314b802c328684f437e62fe23
_InDev_ <aguy867@gmail.com>
2015-06-28 12:34:43 -0400

    Changes

--- src/net/genesishub/gFeaturesBungee/DetermineHub.java
+++
@@ -1,7 +0,0 @@
-package net.genesishub.gFeaturesBungee;
-
-public class DetermineHub {
-	public void determine(Player p){
-		
-	}
-}
---
+++ src/net/genesishub/gFeaturesBungee/event/Join.java
@@ -0,0 +1,10 @@
+package net.genesishub.gFeaturesBungee.event;
+
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+
+public class Join implements Listener{
+	public void onPostLogin(PostLoginEvent event){
+		
+	}
+}
