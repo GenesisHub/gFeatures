5eb3b48ab24b69e6101f3a966dd24b7ffb398d9d
_InDev_ <aguy867@gmail.com>
2015-08-14 09:18:22 -0400

    gFeaturesBungee - 1.0.5

--- src/net/genesishub/gFeaturesBungee/Listeners.java
+++ src/net/genesishub/gFeaturesBungee/Listeners.java
@@ -1,14 +1,19 @@
 package net.genesishub.gFeaturesBungee;
 
-import net.genesishub.gFeaturesBungee.Commands.SlashHub;
+import net.genesishub.gFeaturesBungee.Commands.*;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Listeners extends Plugin{
-	public static String version = "1.0.0";
+	public static String version = "1.0.5";
 	public void onEnable(){
 		getLogger().info("_______________________________________________");
 		getLogger().info("Enabling gFeaturesBungee version " + version + "...");
 		getProxy().getPluginManager().registerCommand(this, new SlashHub());
+		getProxy().getPluginManager().registerCommand(this, new SlashFactions());
+		getProxy().getPluginManager().registerCommand(this, new SlashgWars());
+		getProxy().getPluginManager().registerCommand(this, new SlashSkyblock());
+		getProxy().getPluginManager().registerCommand(this, new SlashSurvival());
+		getProxy().getPluginManager().registerCommand(this, new SlashCreative());
 		getLogger().info("Enabled!");
 		getLogger().info("_______________________________________________");
 	}
